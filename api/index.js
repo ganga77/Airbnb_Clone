a96018900426c0a7738711575b37042c4796c597
ganga77 <gs9831@gmail.com>
2024-02-18 23:15:18 -0500

    Adding Login Page

--- api/index.js
+++ api/index.js
@@ -3,10 +3,12 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
 const User = require('./models/User.js')
 const bcryptSalt = bcrypt.genSaltSync(10); // For Password
+const jwtSecret = 'dgashahdhjajksdsd'; // random value for cookie used in login
 //Connection b/w frontend and backend
 app.use(express.json());
 app.use(cors({
@@ -16,6 +18,7 @@ app.use(cors({
 
 mongoose.connect(process.env.MONGO_URL) // npm i dotenv
 
+//register route
 app.post('/register', async (req, res) => {
     const { name, email, password } = req.body;
     
@@ -30,6 +33,28 @@ app.post('/register', async (req, res) => {
         res.status(422).json(err)
     }
 });
+
+//login route
+app.post('/login', async(req, res) =>{
+    const {email, password} = req.body;
+
+    const userDoc = await User.findOne({email});
+    if(userDoc){
+        const passOk = bcrypt.compareSync(password, userDoc.password);
+            if(passOk){
+                jwt.sign({email: userDoc.email, id: userDoc._id}, jwtSecret, {}, (err, token)=>{
+                    if(err) throw err;
+                    res.cookie('token', token).json('pass ok')
+                });
+            }else{
+                res.status(422).json('pass not ok')
+            }
+        
+    }else{
+        res.json('Not Found')
+    }
+
+})
 
 app.listen(4000, () => {
     console.log('Server is running on port 4000');
